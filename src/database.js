1b98ca7fae4288e208248de055caf1f43f1ea3ad
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2022-01-01 09:19:01 -0300

    Configura e adiciona  variaveis de ambiente no projeto

--- src/database.js
+++ src/database.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 function connectToDatabase() {
-  mongoose.connect('mongodb+srv://gabriel:gbr4864265@cluster0.c1od0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoose.connect(
+    process.env.DATABASE_URL,
   {
       useNewUrlParser: true,
       useUnifiedTopology: true,
   });
-    
+
   const db = mongoose.connection;
   db.on('error', (error) => console.error(error));
   db.once('open', () => console.log('📦 Connected to the database!'))
